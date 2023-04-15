613f6ce6d8b7fd82076c9e1d4a734384cca17d47
王庆堂 <740321314@qq.com>
2023-04-15 19:17:12 +0800

    创建新项目

---
+++ src/main/java/com/tang/yy/YyApplication.java
@@ -0,0 +1,13 @@
+package com.tang.yy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tang/yy/YyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tang.yy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
